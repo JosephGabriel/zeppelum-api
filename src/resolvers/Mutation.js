b7c04f97f48654f08dd6832ed7e26e5df16c1dce
JosephGabriel <josegabriel.jgs18@gmail.com>
2021-03-24 20:30:20 -0300

    Modelagem de dados finalizada

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,7 @@
+import { generateToken, hashPassword, getUserId } from "../../utils/utils";
+
+export const Mutation = {
+  hello() {
+    return "String";
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 import { GraphQLServer } from "graphql-yoga";
 
+import { prisma } from "./prisma";
 import { resolvers } from "./resolvers";
 
 export const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
+  context(request) {
+    return {
+      request,
+      prisma,
+    };
+  },
 });
