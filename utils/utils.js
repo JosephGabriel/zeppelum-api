2685e7611b7922496b8f87431ae72713f2a7cc85
JosephGabriel <josegabriel.jgs18@gmail.com>
2021-03-28 13:33:43 -0300

    Deploy com prisma cloud

--- utils/utils.js
+++ utils/utils.js
@@ -17,7 +17,7 @@ export const hashPassword = (password) => {
 // com validade de 1 semana, o id recebe o nome de payload,
 
 export const generateToken = (payload) => {
-  return jwt.sign({ userId: payload }, "zeppelum", { expiresIn: "7d" });
+  return jwt.sign({ userId: payload }, process.env.PRISMA_AUTH_SECRET);
 };
 
 // a função getUserId recebe 2 parâmetros,
