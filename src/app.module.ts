7a4450db2be4c8af69e48942fef5ee9ee7717545
JosephGabriel <josephgabriel.jgsm@gmail.com>
2023-07-29 21:31:40 -0300

    graphql adicionado, typeorm adicionado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database',
+      entities: ['**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
