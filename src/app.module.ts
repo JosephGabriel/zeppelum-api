01f14aebdd9e748d01fae7a5022b5fc20c92b809
JosephGabriel <josephgabriel.jgsm@gmail.com>
2023-07-29 21:41:36 -0300

    caminhos corrigidos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,18 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: join(__dirname, 'src', 'schema.gql'),
     }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'database',
-      entities: ['**/*.entity{.ts,.js}'],
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
       synchronize: true,
     }),
   ],
