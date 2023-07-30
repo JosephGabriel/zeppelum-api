02863888f9cf16e47773c2d8719deddaa95da8e2
JosephGabriel <josephgabriel.jgsm@gmail.com>
2023-07-30 18:36:11 -0300

    validacao adicionada com class validator

--- src/event/dto/create-event.input.ts
+++ src/event/dto/create-event.input.ts
@@ -1,17 +1,21 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { MinLength, IsUrl } from 'class-validator';
 
 @InputType()
 export class CreateEventInput {
   @Field()
+  @IsUrl()
   image: string;
 
   @Field()
+  @MinLength(3)
   title: string;
 
   @Field()
   price: number;
 
   @Field()
+  @MinLength(20)
   description: string;
 
   @Field()
